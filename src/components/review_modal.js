726c74d8974f999348c5a44b43cc0e23af8b6e7e
Joe Bragel <Bragel@Joes-MBP.attlocal.net>
2021-08-09 14:19:03 -0500

    review modal v0.3.1

--- src/components/review_modal.js
+++ src/components/review_modal.js
@@ -7,21 +7,26 @@ const Review = (props) => {
     return null
   }
 
-  return(                             //clicking outside area of modal will close it
+  return(
+    <>
+    {  props.viewReviewModal === props.data._id &&
     <div className="review-modal-box" onClick={props.onClose}>
       <div className="review-modal-content"
-      //clicking in this area won't close modal
       onClick={event => event.stopPropagation()}>
-        <h2>{props.title}</h2>
+        <h2>{props.data.title}</h2>
         <div className="review-modal-body">
-          Yo Yo Yo this is a review.
+          {props.data.review}
         </div>
         <div className="review-modal-footer">
           <button onClick={props.onClose} className="buttons">Close</button>
         </div>
       </div>
     </div>
-  )
+
+  }
+    </>
+)
+
 }
 
 export default Review
